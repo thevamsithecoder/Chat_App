64326c4e2593199661db0798bb71f27e2c733571
Kotni Vamsi Sai <vamsikotnisai@gmail.com>
2024-11-18 22:13:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+// server.js
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const ChatMessage = require("./models/ChatMessage");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+
+const connectDB = mongoose.connect("mongodb+srv://vamsikotnisai:sgYLGM8IqRaodc6q@cluster0.4ohf9.mongodb.net/chat_app");
+connectDB.then(()=> {
+	console.log("Connected to MongoDB")
+})
+.catch((error)=> {
+	console.log(error, "Error occured while connecting to the database")
+})
+
+connectDB
+
+// Routes
+app.get("/messages", async (req, res) => {
+	try {
+		const messages = await ChatMessage.find();
+		res.json(messages);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+});
+
+app.post("/messages", async (req, res) => {
+	try {
+		const { user, message } = req.body;
+
+		if (!user || !message) {
+			return res
+				.status(400)
+				.json({ error: "User and message are required" });
+		}
+
+		const chatMessage = new ChatMessage({
+			user,
+			message,
+		});
+
+		await chatMessage.save();
+
+		res.status(201).json(chatMessage);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+});
+
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
